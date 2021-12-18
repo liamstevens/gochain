a47c95ab695955cf1deef4d07ba10768a2ed04d7
Liam Stevens <me@liamstevens.me>
2021-12-18 14:36:21 +1100

    copy tutorial code for review

---
+++ src/blockchain.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	// This will join our previous block's relevant info with the new blocks
+	hash := sha256.Sum256(info)
+	//This performs the actual hashing algorithm
+	b.Hash = hash[:]
+	//If this ^ doesn't make sense, you can look up slice defaults
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	//If this is gibberish to you look up
+	// pointer syntax in go
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
