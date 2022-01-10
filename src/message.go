7f86b5d0216ae895f33b3324baaafaae19210f32
Liam Stevens <me@liamstevens.me>
2022-01-10 14:59:36 +1100

    add initial constants for msg type

--- src/message.go
+++ src/message.go
@@ -1,10 +1,16 @@
 package gochain
 
 //Struct for the purpose of constructing messages.
-
 type Message struct {
 	id          int
 	messageType int
 	timestamp   []byte
 	contents    []byte
 }
+
+//constants for messageType usage
+const PING = 0x00
+const PONG = 0x01
+const NEW_LEDGER = 0x03
+const NEW_TX = 0x04
+const AGREEMENT = 0x05
