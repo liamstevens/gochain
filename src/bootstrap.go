6634eccee45d5cae2ce676177b98ef42eefc2fc5
Liam Stevens <me@liamstevens.me>
2022-01-10 14:53:18 +1100

    add bootstrap file - to be populated with functionality for initial network bootstrapping

---
+++ src/bootstrap.go
@@ -0,0 +1,5 @@
+package gochain
+
+//Generate initial UNL - contact curator server and select a random subset of all nodes provided.
+//This ensures a good spread of UNL configurations but does mean that additional configuration
+//should be done if this mechanism is undesired.
