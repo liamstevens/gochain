d7cb75f2ad9a6c000197290ff94e5a1f181f5e1d
Liam Stevens <me@liamstevens.me>
2022-01-11 22:48:28 +1100

    add basic node uid generation function for action on bootstrapping node

--- src/bootstrap.go
+++ src/bootstrap.go
@@ -1,5 +1,30 @@
 package gochain
 
+import (
+	"bufio"
+	"crypto/rand"
+	"fmt"
+	"os"
+)
+
 //Generate initial UNL - contact curator server and select a random subset of all nodes provided.
 //This ensures a good spread of UNL configurations but does mean that additional configuration
 //should be done if this mechanism is undesired.
+
+//Generate Node UID - used for stamping of messages sent by client and server
+func generateNodeUID(filename string) {
+	nodeUID := make([]byte, 32)
+	confFile, ferr := os.Open(filename)
+	if ferr != nil {
+		fmt.Println("error:", ferr)
+		return
+	}
+	_, err := rand.Read(nodeUID)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	w := bufio.NewWriter(confFile)
+	w.Write(nodeUID)
+	confFile.Close()
+}
